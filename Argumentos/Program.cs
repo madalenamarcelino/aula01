2b91374d46e7afc89190dba6f235b88c607d1f29
madalenamarcelino <madelynbennet@gmail.com>
2019-03-01 12:42:08 +0000

    added comments in XML

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -2,11 +2,18 @@
 
 namespace Argumentos
 {
+    /// <summary>
+    /// This program prints what the user writes in the console
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The program starts here
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-           for(int i = 0; i < args.Length; i++)
+            for (int i = 0; i < args.Length; i++)
             {
                 Console.WriteLine(args[i]);
             }
