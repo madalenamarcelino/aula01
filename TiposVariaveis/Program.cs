931078a4125cff1e3a03eeff1e0b6ab017c15b66
madalenamarcelino <madelynbennet@gmail.com>
2019-03-02 14:43:07 +0000

    variables and updates

---
+++ TiposVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TiposVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qual = 1234;
+            float fff = 86f;
+            string val = "44";
+            decimal dec = 33m;
+            char woow = 'h';
+        
+            Console.WriteLine("Os valores sao: "  + qual + " " + fff + " " + val + " " + dec + " " + woow);
+          
+        }
+    }
+}
